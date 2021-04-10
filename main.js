3f5d28b80c167416003081aa634473f66c20198f
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-04-10 00:53:06 -0400

    commit tree e3673a40e2c1dcf28d8a36e0fbebbdc6a672592b

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const errorBanner = document.querySelector('#modal')
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', () => {
+  errorBanner.classList.add("hidden")
+  const likeHearts = document.querySelectorAll(".like-glyph")
+  for (const glyph of likeHearts){
+    glyph.addEventListener('click', handleHeartClick)
+  }
+})
 
+function handleHeartClick(event){
+  const heart = event.target
+  mimicServerCall()
+  .then(() => {
+    if(heart.innerText === EMPTY_HEART){
+      heart.innerText = FULL_HEART
+      heart.classList.add("activated-heart")
+    } else if(heart.innerText === FULL_HEART){
+      heart.innerText = EMPTY_HEART
+      heart.classList.remove("activated-heart")
+    }
+  })
+  .catch(error => {
+    errorBanner.classList.remove("hidden")
+    errorBanner.innerText = error
+    setTimeout(function(){errorBanner.classList.add("hidden")}, 5000)
+  })
+}
 
 
 
